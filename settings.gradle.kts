ef3230df42487029ed0bab1a8894d1e6cadeb6c2
Guilherme Lourenco <guilherme.lourenco@docket.com.br>
2023-07-06 10:33:12 -0300

    chore: Feature

--- settings.gradle.kts
+++
@@ -1,6 +0,0 @@
-rootProject.name = "poc-reactive"
-include("services")
-include("services:api-spring-factory")
-findProject(":services:api-spring-factory")?.name = "api-spring-factory"
-include("services:api-spring-reactive")
-findProject(":services:api-spring-reactive")?.name = "api-spring-reactive"
