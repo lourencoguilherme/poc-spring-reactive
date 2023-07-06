52df4f85e8c1baa11b4f18d8d8a84be8ac14c12f
Guilherme Lourenco <guilherme.lourenco@docket.com.br>
2023-07-06 09:33:45 -0300

    Add docker-compose mockup-configuration

--- services/api-spring-reactive/src/main/kotlin/com/example/springbootkotlinreactiveexample/SpringBootKotlinReactiveApplication.kt
+++ services/api-spring-reactive/src/main/kotlin/com/example/springbootkotlinreactiveexample/SpringBootKotlinReactiveApplication.kt
@@ -1,17 +1,8 @@
 package com.example.springbootkotlinreactiveexample
 
-import com.example.springbootkotlinreactiveexample.dto.ErrorDTO
-import com.example.springbootkotlinreactiveexample.dto.TodoDTO
-import org.springframework.aot.hint.annotation.RegisterReflectionForBinding
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@RegisterReflectionForBinding(
-    classes = [
-        TodoDTO::class,
-        ErrorDTO::class,
-    ],
-)
 @SpringBootApplication
 class SpringBootKotlinReactiveApplication
 
